43bcb0c919ee5630c64e580aa1b776ff0f1d5b4f
dannydelott <dannydelott@gmail.com>
2015-03-26 09:48:31 -0700

    Update class and ID names
    - Change subreddit-xxx-xxx to reddit-xxx-xxx.
    - Use dashes instead of underscores in template IDs.

--- js/views/RedditInputView.js
+++ js/views/RedditInputView.js
@@ -1,15 +1,15 @@
 window.RedditInputView = Backbone.View.extend( {
 
   el: $( '<div>', {
-    id: 'subreddit-input-container'
+    id: 'reddit-input-container'
   } ),
 
-  template: _.template( $( '#subreddit_input_template' ).html() ),
+  template: _.template( $( '#reddit-input-template' ).html() ),
 
   initialize: function() {
     this.render();
   },
-  // Render the template and fade it in.
+
   render: function() {
     this.$el.html( this.template ).prependTo( document.body );
   }
