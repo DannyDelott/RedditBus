ff717cde684f5b913942e5e952b90cd53d401b00
dannydelott <dannydelott@gmail.com>
2015-03-28 00:06:14 -0700

    Add defaults to Subreddit model

--- js/models/Subreddit.js
+++ js/models/Subreddit.js
@@ -1,7 +1,18 @@
 window.Subreddit = Backbone.Model.extend( {
 
+  defaults: {
+    subreddit: 'explainlikeimfive',
+    day: 'Thursday',
+    time: 'Fri Mar 27 2015 21:58:29 GMT-0700 (PDT)',
+    data: {
+      posts: {}
+    },
+
+  },
+
   initialize: function() {
 
+    console.log( 'subreddit model' );
   }
 
 } );
