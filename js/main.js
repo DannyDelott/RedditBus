4edff92eb5878b52852cff10eb4c28122f0155fc
dannydelott <dannydelott@gmail.com>
2015-03-28 00:02:51 -0700

    Remove views from main.js
    - LogoView, RedditInputView, CreditsView.

--- js/main.js
+++ js/main.js
@@ -8,10 +8,4 @@ var subreddit = new Subreddit();
  * VIEWS *
  * *******/
 
-var logoView = new LogoView();
-
-var inputView = new RedditInputView( {
-  model: subreddit
-} );
-
-var creditsView = new CreditsView();
+var centerBoxView = new CenterBoxView();
