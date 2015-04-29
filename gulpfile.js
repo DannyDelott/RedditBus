6d83c9e7b3fad06e986398367e6ef4543cc79363
dannydelott <dannydelott@gmail.com>
2015-04-28 23:16:51 -0700

    Update gulpfile.js to build dist code
    - Add gulp task to uglify and concatenate javascript files.
    - Add default task.
    - Update index.html to use dist/ javascript.

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,65 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 
-var path = {
-  ENTRY_POINT_JS:'./client/js/**/*.js',
-  MINIFIED_OUT: './client/dist/'
-};
+/* **************************************************
+ * PATH object contains the filepaths to site assets.
+ *
+ * NOTES:
+ * APP and LIBS use arrays to maintain order when
+ * concatenating mulitiple files into one.
+ * **************************************************/
+
+var path = {};
+
+// Backbone App
+path.APP_DIR = './client/js/';
+path.APP = [
+  path.APP_DIR + 'models/Subreddit.js',
+  path.APP_DIR + 'views/CenterBoxView.js',
+  path.APP_DIR + 'views/LogoView.js',
+  path.APP_DIR + 'views/RedditInputView.js',
+  path.APP_DIR + 'views/CreditsView.js',
+  path.APP_DIR + 'AppRouter.js',
+  path.APP_DIR + 'app.js'
+];
+
+// 3rd party libraries
+path.LIBS_DIR = './client/lib/';
+path.LIBS = [
+  path.LIBS_DIR + 'underscore-min.js',
+  path.LIBS_DIR + 'jquery-min.js',
+  path.LIBS_DIR + 'backbone-min.js'
+];
+
+// Distribution assets
+path.CLIENT_DIST = './client/dist/';
+path.APP_MIN = path.CLIENT_DIST + 'app.min.js';
+path.LIBS_MIN = path.CLIENT_DIST + 'lib.min.js';
+
+/* **************************************************
+ * UGLIFY AND CONCATENATE task compresses JavaScript
+ * files then combines them into one file that gets
+ * served to the client.
+ * **************************************************/
 
 gulp.task('js', function() {
 
-      // main app js file
-      gulp.src(path.ENTRY_POINT_JS)
-      .pipe(uglify())
-      .pipe(concat("app.min.js"))
-      .pipe(gulp.dest(path.MINIFIED_OUT));
+  // backbone app
+  gulp.src(path.APP)
+    .pipe(uglify())
+    .pipe(concat(path.APP_MIN))
+    .pipe(gulp.dest(path.CLIENT_DIST));
 
-      // create 1 vendor.js file from all vendor plugin code
-      // gulp.src('./assets/js/vendor/**/*.js')
-      // .pipe(uglify())
-      // .pipe(concat("vendor.js"))
-      // .pipe(gulp.dest('./assets/js'))
-      // .pipe( notify({ message: "Javascript is now ugly!"}) );
-    });
+  // 3rd party libraries
+  gulp.src(path.LIBS)
+    .pipe(concat(path.LIBS_MIN))
+    .pipe(gulp.dest(path.CLIENT_DIST));
+});
+
+/* **************************************************
+ * DEFAULT task runs the necessary gulp tasks in the
+ * correct order to build distribution code that gets
+ * served to the client.
+ * **************************************************/
+
+gulp.task('default', ['js']);
